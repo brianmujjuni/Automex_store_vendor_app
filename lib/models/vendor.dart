d42f4be32ec8eab846369523bd99ffe4cb9cb6ff
brianmujjuni <brianmujjuni@engineer.com>
2025-05-12 00:17:26 +0300

    create Vendor model and mvc file stracture

---
+++ lib/models/vendor.dart
@@ -0,0 +1,55 @@
+import 'dart:convert';
+
+class Vendor {
+  final String id;
+  final String fullName;
+  final String email;
+  final String state;
+  final String city;
+  final String locality;
+  final String role;
+  final String password;
+
+  Vendor({
+    required this.id,
+    required this.fullName,
+    required this.email,
+    required this.state,
+    required this.city,
+    required this.locality,
+    required this.role,
+    required this.password,
+  });
+// convertint to map so we can easily convert to json
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'fullName': fullName,
+      'email': email,
+      'state': state,
+      'city': city,
+      'locality': locality,
+      'role': role,
+      'password': password,
+    };
+  }
+
+  // convertint to json so we can easily convert to map
+  String toJson() => json.encode(toMap());
+
+// convertint from map to vendor
+  factory Vendor.fromMap(Map<String, dynamic> map) {
+    return Vendor(
+      id: map['_id'] ?? '',
+      fullName: map['fullName'] ?? '',
+      email: map['email'] ?? '',
+      state: map['state'] ?? '',
+      city: map['city'] ?? '',
+      locality: map['locality'] ?? '',
+      role: map['role'] ?? '',
+      password: map['password'] ?? '',
+    );
+  }
+
+  // factory Vendor.fromJson(String source) => Vendor.fromMap(json.decode(source) as Map<String, dynamic>);  
+}
