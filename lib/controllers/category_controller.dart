0739ae68a520aab634ff42a213e0b0332a9925b6
brianmujjuni <brianmujjuni@engineer.com>
2025-05-23 11:03:03 +0300

    create category controller and fetch categories function

---
+++ lib/controllers/category_controller.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+import 'package:automex_store_vendor/global_variables.dart';
+import 'package:automex_store_vendor/models/category.dart';
+import 'package:http/http.dart' as http;
+
+class CategoryController {
+  
+  //fetch categories from server
+  Future<List<Category>> fetchCategories() async {
+    try {
+      final response = await http.get(Uri.parse("$uri/api/categories"),
+          headers: <String, String>{
+            "Content-Type": "application/json; charset=UTF-8"
+          });
+          print(response.body);
+          
+      if (response.statusCode == 200) {
+        final List<dynamic> data = jsonDecode(response.body);
+        List<Category> categories =
+            data.map((category) => Category.fromJson(category)).toList();
+        return categories;
+      }else{
+        throw Exception("Failed to load categories");
+      }
+    } catch (err) {
+      throw Exception("Error loading categories: $err");
+    }
+  }
+}
