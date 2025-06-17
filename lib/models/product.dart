c7d9bce27eb5cf51c8801e79fb28bcc144496307
brianmujjuni <brianmujjuni@engineer.com>
2025-06-17 14:40:29 +0300

    format the product data

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -10,5 +10,44 @@ class Product {
   final String vendorId;
   final String fullName;
 
-  Product({required this.id, required this.productName, required this.productPrice, required this.quantity, required this.description, required this.category, required this.subCategory, required this.images, required this.vendorId, required this.fullName});
+  Product(
+      {required this.id,
+      required this.productName,
+      required this.productPrice,
+      required this.quantity,
+      required this.description,
+      required this.category,
+      required this.subCategory,
+      required this.images,
+      required this.vendorId,
+      required this.fullName});
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'productName': productName,
+      'productPrice': productPrice,
+      'quantity': quantity,
+      'description': description,
+      'category': category,
+      'subCategory': subCategory,
+      'images': images,
+      'vendorId': vendorId,
+      'fullName': fullName
+    };
+  }
+  factory Product.fromMap(Map<String,dynamic>map){
+    return Product(
+      id: map['_id'] as String,
+      productName: map['productName'] as String,
+      productPrice: map['productPrice'] as int,
+      quantity: map['quantity'] as int,
+      description: map['description'] as String,
+      category: map['category'] as String,
+      subCategory: map['subCategory'] as String,
+      images:  map['images'] as String,
+      vendorId: map['vendorId'] as String,
+      fullName: map['fullName'] as String
+    );
+  }
 }
