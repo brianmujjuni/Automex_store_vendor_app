97a1df209571526bc0ee8fe2de4a3eb975563b21
brianmujjuni <brianmujjuni@engineer.com>
2025-06-17 14:13:45 +0300

    create product class and construcor

---
+++ lib/models/product.dart
@@ -0,0 +1,14 @@
+class Product {
+  final String id;
+  final String productName;
+  final int productPrice;
+  final int quantity;
+  final String description;
+  final String category;
+  final String subCategory;
+  final String images;
+  final String vendorId;
+  final String fullName;
+
+  Product({required this.id, required this.productName, required this.productPrice, required this.quantity, required this.description, required this.category, required this.subCategory, required this.images, required this.vendorId, required this.fullName});
+}
