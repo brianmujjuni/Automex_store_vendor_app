f32e5518190b803b4731045e7b45c247db503078
brianmujjuni <brianmujjuni@engineer.com>
2025-06-17 15:10:54 +0300

    create functionality to upload images to cloudinary in product controller

--- lib/controllers/product_controller.dart
+++ lib/controllers/product_controller.dart
@@ -1,13 +1,32 @@
+import 'dart:io';
+
+import 'package:cloudinary_public/cloudinary_public.dart';
+
 class ProductController {
-  void uploadProduct({
-    required String productName,
-    required int productPrice,
-    required int quantity,
-    required String description,
-    required String category,
-    required String subCategory,
-    required String images,
-    required String vendorId,
-    required String fullName
-  }) {}
+  void uploadProduct(
+      {required String productName,
+      required int productPrice,
+      required int quantity,
+      required String description,
+      required String category,
+      required String subCategory,
+      required List<File>? pickedImages,
+      required String vendorId,
+      required String fullName}) async {
+    if (pickedImages != null) {
+      final cloudinary = CloudinaryPublic("automex", "automex store");
+      List<String> images = [];
+      //loop through each image on the pickedimage list
+      for (var i = 0; i < pickedImages.length; i++) {
+        //await the upload of the current image to cloudinary
+        CloudinaryResponse cloudinaryResponse = await cloudinary.uploadFile(
+          CloudinaryFile.fromFile(pickedImages[i].path, folder: productName),
+        );
+        //add the secure url to the images list
+
+        images.add(cloudinaryResponse.secureUrl);
+      }
+      print(images);
+    }
+  }
 }
