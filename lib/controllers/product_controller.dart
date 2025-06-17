3cd7c345f26d086d01888a89c3d8f1d7beb3bd7e
brianmujjuni <brianmujjuni@engineer.com>
2025-06-17 18:15:33 +0300

    handle http on create product in Products controller

--- lib/controllers/product_controller.dart
+++ lib/controllers/product_controller.dart
@@ -1,5 +1,8 @@
 import 'dart:io';
-
+import 'package:automex_store_vendor/global_variables.dart';
+import 'package:http/http.dart' as http;
+import 'package:automex_store_vendor/models/product.dart';
+import 'package:automex_store_vendor/services/manage_http_response.dart';
 import 'package:cloudinary_public/cloudinary_public.dart';
 
 class ProductController {
@@ -12,7 +15,8 @@ class ProductController {
       required String subCategory,
       required List<File>? pickedImages,
       required String vendorId,
-      required String fullName}) async {
+      required String fullName,
+      required context}) async {
     if (pickedImages != null) {
       final cloudinary = CloudinaryPublic("automex", "automex store");
       List<String> images = [];
@@ -26,7 +30,37 @@ class ProductController {
 
         images.add(cloudinaryResponse.secureUrl);
       }
-      print(images);
+      if (category.isNotEmpty && subCategory.isNotEmpty) {
+        final Product product = Product(
+          id: '',
+          productName: productName,
+          productPrice: productPrice,
+          quantity: quantity,
+          description: description,
+          category: category,
+          subCategory: subCategory,
+          images: images,
+          vendorId: vendorId,
+          fullName: fullName,
+        );
+        http.Response response = await http.post(
+          Uri.parse("$uri/add-product"),
+          body: product.toJson(),
+          headers: <String, String>{
+            "Content-Type": "application/json; charset-UTF-8",
+          },
+        );
+        manageHttpResponse(
+            response: response,
+            context: context,
+            onSuccess: () {
+              showSnackBar(context, 'Product saved successfully');
+            });
+      } else {
+        showSnackBar(context, 'Fill all fields');
+      }
+    } else {
+      showSnackBar(context, 'Select image');
     }
   }
 }
